ee359c05998825e4db119eed5f9afe70c7c3b5bc
tobias.keel <tobias.keel@bluewin.ch>
2026-08-03 17:36:29 +0200

    correct mppt addrs

--- SER Live Monitoring/Models/CanConfig.cs
+++ SER Live Monitoring/Models/CanConfig.cs
@@ -2,9 +2,9 @@
 
 public class CanConfig
 {
-    public static readonly short Mppt1Addr = 0x600;
-    public static readonly short Mppt2Addr = 0x610;
-    public static readonly short Mppt3Addr = 0x620;
+    public static readonly short Mppt1Addr = 0x6A0;
+    public static readonly short Mppt2Addr = 0x6B0;
+    public static readonly short Mppt3Addr = 0x6C0;
     public static readonly short BmsBaseAddr = 0x700;
     public static readonly short AcBaseAddr = 0x630;
     public static readonly short DcBaseAddr = 0x660;
